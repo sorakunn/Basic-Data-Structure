7154a35a5390c98c6bd2e2a64f84950271ed9415
valliere <sorasunn111@gmail.com>
2020-05-24 15:03:14 +0900

    打印单链表的所有元素

--- Singly Linked List/1. Main.cpp
+++ Singly Linked List/1. Main.cpp
@@ -11,10 +11,11 @@ int main()
 	CreateHead(head, date, 7);						// 通过头插法构造一个单链表
 	Print(p);										// 输出结果：	{6,5,4,3,2,1,0}
 
-	//Elem e; 													// 删除并取出下标为0位置上的元素			Date[]{1,2,3,4,5,6,7,8,9}
+	//Elem e; 										// 删除并取出下标为0位置上的元素			Date[]{1,2,3,4,5,6,7,8,9}
 	//DeleteElem(l, 0, e);
 	//Print(l);
 
-	//printf("%d", LocateElem(l, 9));							// 查找值为9的元素所在的下标位置			8
+	//printf("%d", LocateElem(l, 9));				// 查找值为9的元素所在的下标位置			8
+	printf("%d", GetElem(p, 7)->Date);				// 查找第一个结点内元素的值				0
 	return 0;
 }
