7a4fc7fb0b6db9ce1133ab5b88ab87fd291dcc6c
valliere <sorasunn111@gmail.com>
2020-05-24 16:41:38 +0900

    查找位置index的结点并返回其指针

--- Singly Linked List/0. Typedef.h
+++ Singly Linked List/0. Typedef.h
@@ -10,7 +10,7 @@ typedef struct SLNode{
 
 bool InsertElem(SLNode &head, int p, Elem x);			// 挿入（在指定下标位置处挿入所给元素）
 bool DeleteElem(SLNode &head, int p, Elem x);			// 削除（删除并取出指定下标位置上的数）
-SLNode LocateElem(SLNode head, Elem x);					// 探索（查找值为x的第一个Elem元素并返回其指针）
+SLNode* LocateElem(SLNode* head, Elem x);				// 探索（查找值为x的第一个Elem元素并返回其指针）
 
 void CreateHead(SLNode *head, Elem x[], int n);			// 先頭への挿入（从后往前逆向建立单链表，每次将新元素插在"头指针后"）
 void CreateTail(SLNode *head, Elem x[], int n);			// 後尾への挿入（从前往后正向建立单链表，每次将新元素插在"表尾"）
--- Singly Linked List/4. Locate.cpp
+++ Singly Linked List/4. Locate.cpp
@@ -1,13 +1,13 @@
 #include "0. Typedef.h"
 /*********************************************************************************
   *Function:  		LocateElem
-  *Description：		查找值为x的第一个Elem元素并返回其下标
-					/Sqlist *list/ 指向待执行查找操作线性表的指针
-					/Elem x/ 指向用来存放取出元素的空Elem元素的指针
+  *Description：		查找Elem元素值为x的第一个结点并返回其指针
+					SLNode* head		指向待执行查找操作线性表的指针
+					Elem x				 指向用来存放取出元素的空Elem元素的指针
   *Output:  none
   *Return:  index of target Elem element
 **********************************************************************************/
-int LocateElem(SqList list, Elem x) {
+SLNode* LocateElem(SLNode* head, Elem x){
 	for (int i = 0; i < list.length; i++) {
 		if (list.Date[i] == x) return i;
 	}
