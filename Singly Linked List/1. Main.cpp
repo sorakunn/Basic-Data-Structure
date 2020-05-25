cfede5f2d8ef2081b6fa3cb6ce31c4f9f0359a0c
valliere <sorasunn111@gmail.com>
2020-05-25 10:20:17 +0900

    在单链表的指定结点index处，挿入包含所给元素x的新结点

--- Singly Linked List/1. Main.cpp
+++ Singly Linked List/1. Main.cpp
@@ -13,12 +13,14 @@ int main()
 	CreateTail(head, date, 5);						// 通过 "尾插法" 构造一个单链表
 	Print(p);										// 输出结果：	{1,2,3,4,5}
 
-	//Elem e; 										// 删除并取出下标为0位置上的元素			Date[]{1,2,3,4,5,6,7,8,9}
+	InsertElem(p, 4, 3);							// 在第4个结点处，挿入包含元素3的新结点
+	Print(p);
+													// 删除并取出下标为0位置上的元素			Date[]{1,2,3,4,5,6,7,8,9}
 	//DeleteElem(l, 0, e);
 	//Print(l);
 
-	printf("%d\n", LocateElem(p, 5));				// 查找值为5的元素所在的结点指针
-	printf("%d\n", LocateElem(p, 6));				// 查找不存在元素所在的结点指针			NULL
+	printf("%p\n", LocateElem(p, 5));				// 查找值为5的元素所在的结点指针
+	printf("%p\n", LocateElem(p, 7));				// 查找不存在元素所在的结点指针			NULL
 	printf("%d\n", GetElem(p, 3)->Date);			// 查找第3个结点内元素的值				3
 	return 0;
 }
