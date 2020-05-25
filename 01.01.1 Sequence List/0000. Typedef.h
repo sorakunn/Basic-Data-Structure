ad8987978466065f4f7578d364cc8f18d11173ea
zlw <sorakunn@users.noreply.github.com>
2020-05-25 15:51:44 +0900

    挿入、削除、探索

--- 01.01.1 Sequence List/0000. Typedef.h
+++ 01.01.1 Sequence List/0000. Typedef.h
@@ -2,7 +2,7 @@
 #include <malloc.h>
 #include <stdbool.h>
 #define MAXSIZE 50 		// MAXSIZE：リストの容量（定数である）
-typedef int Elem;		// Elem：目標のデータの型 
+typedef int Elem;		// Elem：目標のデータの型
 
 /* "顺序表"的定义 */
 typedef struct SqList{
@@ -14,5 +14,4 @@ typedef struct SqList{
 bool InsertElem(SqList &list, int index, Elem x);	// 挿入（在顺序表的指定下标index处挿入所给元素x）
 bool DeleteElem(SqList &list, int index);		// 削除（删除顺序表的指定下标index上的元素）
 int LocateElem(SqList list, Elem x);			// 探索（查找值为x的第一个元素并返回其下标）
-
 void Print(SqList list); 				// 出力（打印顺序表的所有元素）
