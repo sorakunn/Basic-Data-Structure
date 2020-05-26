9f623aa8738a73301fd259f7153252169b9ac110
zlw <sorakunn@users.noreply.github.com>
2020-05-26 15:08:29 +0900

    "双链表" 的定义和基本操作

--- 01.03.1 Doubly Linked List/0010. Main.cpp
+++ 01.03.1 Doubly Linked List/0010. Main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	DLNode *head;						// 初始化一个头指针
+	DLNode *head;						// 初始化一个头指针 
 	head = (DLNode *)malloc(sizeof(DLNode));
 	head->prior = NULL;
 	head->next = NULL;
@@ -10,19 +10,19 @@ int main()
 	/* 头插法建表 */
 	CreateHead(head, date, 5);				// 通过 "头插法" 构造一个双链表
 	Print(head);						// 输出结果：	{5,4,3,2,1}
-	///* 尾插法建表 */
+	/* 尾插法建表 */
 	CreateTail(head, date, 5);				// 通过 "尾插法" 构造一个双链表
 	Print(head);						// 输出结果：	{1,2,3,4,5}
-	///* 插入结点 */
+	/* 插入结点 */
 	InsertElem(head, 4, 3);					// 在第4个结点处，挿入包含元素3的新结点
 	Print(head);						// 输出结果：	{1,2,3,3,4,5}
-	///* 删除结点 */
+	/* 删除结点 */
 	DeleteElem(head, 5);					// 删除双链表的第5个结点
 	Print(head);						// 输出结果：	{1,2,3,3,5}
-	///* 按值查找 */
+	/* 按值查找 */
 	printf("%p\n", LocateElem(head, 5));			// 查找值为5的元素所在的结点指针		每次实时结果不同
 	printf("%p\n", LocateElem(head, 4));			// 查找不存在元素所在的结点指针		NULL
-	///* 按序号查找 */
+	/* 按序号查找 */
 	printf("%d\n", GetElem(head, 4)->Date);			// 查找第4个结点内元素的值		3
 	return 0;
 }
