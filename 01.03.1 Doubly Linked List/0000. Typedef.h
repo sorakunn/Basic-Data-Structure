22f351299f3af7a0172e1468f3a90dcebbce8b35
zlw <sorakunn@users.noreply.github.com>
2020-05-26 11:41:07 +0900

    查找指定序号index的结点并返回其指针

--- 01.03.1 Doubly Linked List/0000. Typedef.h
+++ 01.03.1 Doubly Linked List/0000. Typedef.h
@@ -16,5 +16,5 @@ void CreateHead(DLNode *head, Elem x[], int n);		// 先頭への挿入（从后往前逆向建
 void CreateTail(DLNode *head, Elem x[], int n);		// 後尾への挿入（从前往后正向建立双链表，每次将新元素插在"表尾"）
 //bool DeleteElem(DLNode *head, int index);		// 削除（删除双链表的指定序号index位置的结点）
 DLNode* LocateElem(DLNode* head, Elem x);		// 探索（查找值为x的第一个结点并返回其指针）
-//DLNode* GetElem(DLNode *head, int index);		// 取出す（查找指定序号index的结点并返回其指针
+DLNode* GetElem(DLNode *head, int index);		// 取出す（查找指定序号index的结点并返回其指针
 void Print(DLNode *head); 				// 出力（打印双链表的所有元素）
--- 01.03.1 Doubly Linked List/0041. Get.cpp
+++ 01.03.1 Doubly Linked List/0041. Get.cpp
@@ -2,10 +2,21 @@
 /*********************************************************************************
   *函数： GetElem
   *描述： 查找指定序号index的结点并返回其指针
-  *输入：	SLNode *head		待执行查找操作链表的头指针
+  *输入：	DLNode *head		待执行查找操作双链表的头指针
 		int index		目标结点的序号index
   *输出： 无
-  *返回： 	SLNode* pointer		目标结点的指针
-		SLNode* head		头指针
+  *返回： 	DLNode* pointer		目标结点的指针
+		DLNode* head		头指针
 		NULL			查找失败的反馈
 **********************************************************************************/
+DLNode* GetElem(DLNode *head, int index) {
+	if (index < 0) return NULL;				// 1.查找位置 "在头节点之前"
+	if (index == 0) return head;				// 2.查找位置为 "头节点"
+	DLNode *p = head;
+	for (int i = 0; i < index; i++) {			/* 判断是否已到达 "目标位置"*/
+		if (p == NULL) break;				/* 判断是否已到达 "表尾" */
+		p = p->next;
+	}
+	return p;
+
+}
