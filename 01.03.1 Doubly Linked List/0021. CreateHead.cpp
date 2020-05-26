8995a706ca2896d7256ef67a0cce881c0fc2c4fa
zlw <sorakunn@users.noreply.github.com>
2020-05-26 10:17:36 +0900

    从后往前逆向建立双链表，每次将新元素插在"头指针后"

--- 01.03.1 Doubly Linked List/0021. CreateHead.cpp
+++ 01.03.1 Doubly Linked List/0021. CreateHead.cpp
@@ -10,13 +10,15 @@
 **********************************************************************************/
 void CreateHead(DLNode *head, Elem x[], int n) {
 	for (int i = 0; i < n; i++) {
-		DLNode *inserted = (DLNode*)malloc(sizeof(DLNode));
-		inserted->Date = x[i];
-		inserted->prior = head;
-		inserted->next = head->next;
-		head->next = inserted;
-		if (inserted->next != NULL) {
-			inserted->next->prior = inserted;
+		DLNode *temp = (DLNode*)malloc(sizeof(DLNode));	/* 建立存放数据的结点 */
+		temp->Date = x[i];
+		temp->prior = head;
+		// 先连后再连前, 否则会丢失后一个结点的地址信息
+		temp->next = head->next;			/* 新结点的尾放在 "第一个元素前" */ 
+		head->next = temp;				/* 新结点的头放在 "头指针后" */
+		// 如果后元素存在，才需要进行后元素的前指针修改
+		if (temp->next != NULL) {			/* 判断是否需要修改后元素的前指针*/
+			temp->next->prior = temp;
 		}
 	}
 }
