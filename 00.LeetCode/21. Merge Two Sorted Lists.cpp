1477ba632c6cf63ca3e4f4beceb147aa35332f0d
zlw <sorakunn@users.noreply.github.com>
2020-05-27 17:19:19 +0900

    Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.

---
+++ 00.LeetCode/21. Merge Two Sorted Lists.cpp
@@ -0,0 +1,45 @@
+﻿#include"SLkList.h"
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
+	struct ListNode *head, *p, *p1, *p2;
+	head = (ListNode*)malloc(sizeof(ListNode));
+	head->next = NULL;
+	p = head, p1 = l1, p2 = l2;
+	while (p1 && p2) {				/* 作比较，将较小结点拼上，直至其中一个到尾 */
+		if (p1->val <= p2->val) {
+			// 不必申请新结点，直接拆结点拼接
+			p->next = p1;
+			p1 = p1->next;			/* "链表l1"被拆掉一个结点时，头指针p1后移一格 */
+		}
+		else {
+			// 不必申请新结点，直接拆结点拼接
+			p->next = p2;
+			p2 = p2->next;			/* "链表l2"被拆掉一个结点时，头指针p2后移一格 */
+		}
+		// 两种情况的"新链表头指针p"的后移，可统一为一句来写
+		p = p->next;				/* "存放结果的链表"增加一个结点，头指针p后移一格 */
+	}
+	// 剩下的部分不用逐个拼，整截连上就好
+	if (p1) p->next = p1;
+	if (p2) p->next = p2;
+	return head->next;
+}
+int main() {
+	struct ListNode *l1 = (ListNode*)malloc(sizeof(ListNode));
+	struct ListNode *l2 = (ListNode*)malloc(sizeof(ListNode));
+	int x1[] = { 1,2,4 };
+	int x2[] = { 1,3,4 };
+	l1=CreateTail(l1, x1, 3);
+	l2=CreateTail(l2, x2, 3);
+	Print(l1);
+	Print(l2);
+	Print(mergeTwoLists(l1, l2));
+}
---
+++ 00.LeetCode/SLkList.h
@@ -0,0 +1,27 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+
+struct ListNode {
+	int val;
+	struct ListNode *next;
+};
+
+ListNode* CreateTail(ListNode *head, int x[], int n) {					/*建立测试数据*/
+	ListNode *p = head;
+	for (int i = 0; i < n; i++) {
+		ListNode *temp = (ListNode*)malloc(sizeof(ListNode));
+		temp->val = x[i];
+		temp->next = NULL;
+		p->next = temp;
+		p = temp;
+	}
+	return head->next;
+}
+void Print(ListNode *head) {								/*打印输出功能*/
+	ListNode *p = head;
+	while (p) {
+		printf("%d ", p->val);
+		p = p->next;
+	}
+	printf("\n");
+}
