3def5fe3f488307bffe5e6bfa958f64e6156727d
zlw <sorakunn@users.noreply.github.com>
2020-06-14 11:12:30 +0900

    在顺序栈的栈顶挿入所给元素x

---
+++ 02.01.1 Sequence Stack/0020. Push.cpp
@@ -0,0 +1,15 @@
+﻿#include "0000. Typedef.h"
+/*********************************************************************************
+  *函数： Push
+  *描述： 在顺序栈的栈顶挿入所给元素x
+  *输入：	SqStack &stack		待执行进栈操作的顺序栈的引用类型&stack
+		Elem x			待入栈的目标元素x
+  *输出： 已进栈指定元素的顺序栈引用类型&stack
+  *返回：	bool true		入栈成功的反馈
+		bool false		入栈失败的反馈
+**********************************************************************************/
+bool Push(SqStack &stack, Elem x) {
+	if (stack.top == MAXSIZE-1) return false;		// 栈满：栈顶下标 = MAXSIZE-1
+	stack.Date[++stack.top] = x;				/* 栈顶自增 && 插入数据 */
+	return true;
+}
