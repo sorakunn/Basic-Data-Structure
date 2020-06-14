f9fc16502d683428365e378b1f797ed88d117462
zlw <sorakunn@users.noreply.github.com>
2020-06-14 11:20:27 +0900

    取出并返回顺序栈的栈顶元素x

--- 02.01.1 Sequence Stack/0000. Typedef.h
+++ 02.01.1 Sequence Stack/0000. Typedef.h
@@ -1,7 +1,7 @@
 ﻿#include <stdio.h>
 #include <malloc.h>
 #include <stdbool.h>
-#define MAXSIZE 50 		// MAXSIZE：スタックの容量（定数である）
+#define MAXSIZE 50 		// MAXSIZE：スタックの容量（定数である） 
 typedef int Elem;		// Elem：目標のデータの型
 
 /* "顺序栈"的定义 */
@@ -12,5 +12,5 @@ typedef struct SqStack {
 
 /* "顺序栈"的基本操作 */
 bool Push(SqStack &stack, Elem x);			// 先頭から要素を挿入（在顺序栈的栈顶挿入所给元素x）
-bool Pop(SqStack &stack, Elem &x);			// 先頭から要素を削除（取出并返回顺序栈的栈顶元素值x）
+bool Pop(SqStack &stack, Elem &x);			// 先頭から要素を削除（取出并返回顺序栈的栈顶元素x）
 void Print(SqStack stack); 				// 出力（打印顺序栈的所有元素）
