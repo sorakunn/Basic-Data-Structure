bf2c321a2709b7bafdecfeb8a25016ca2d7b48b5
zlw <sorakunn@users.noreply.github.com>
2020-06-19 08:40:55 +0900

    入れる、出す

--- 02.02.1 Linked Stack/0000. Typedef.h
+++ 02.02.1 Linked Stack/0000. Typedef.h
@@ -1,7 +1,7 @@
 ﻿#include <stdio.h>
 #include <malloc.h>
 #include <stdbool.h>
-typedef int Elem;		// Elem：目標のデータの型 
+typedef int Elem;		// Elem：目標のデータの型
 
 /* "链栈"的定义 */
 typedef struct LNode {
@@ -10,6 +10,6 @@ typedef struct LNode {
 }LNode;
 
 /* "链栈"的基本操作 */
-bool Push(LNode *head, Elem x);				// 先頭から要素を挿入（在顺序栈的栈顶挿入所给元素x）
-bool Pop(LNode *head, Elem &x);				// 先頭から要素を削除（取出并返回顺序栈的栈顶元素x）
-void Print(LNode *head); 				// 出力（打印顺序栈的所有元素）
+bool Push(LNode *head, Elem x);				// 先頭から要素を挿入（在链栈的栈顶挿入所给元素x）
+bool Pop(LNode *head, Elem &x);				// 先頭から要素を削除（取出并返回链栈的栈顶元素x）
+void Print(LNode *head); 				// 出力（打印链栈的所有元素）
