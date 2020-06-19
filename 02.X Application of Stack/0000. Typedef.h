3b65e6651f1a819bebe47a12cd73c10cab770489
zlw <sorakunn@users.noreply.github.com>
2020-06-19 18:02:53 +0900

    利用顺序栈翻转给定的字符串sring

---
+++ 02.X Application of Stack/0000. Typedef.h
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>
+#include <malloc.h>
+#include <stdbool.h>
+
+
+/* "链栈"的基本操作 */
+bool Push(LNode *head, Elem x);				// 先頭から要素を挿入（在链栈的栈顶挿入所给元素x）
+bool Pop(LNode *head, Elem &x);				// 先頭から要素を削除（取出并返回链栈的栈顶元素x）
+void Print(LNode *head); 				// 出力（打印链栈的所有元素）
---
+++ 02.X Application of Stack/0001. ReverseString.cpp
@@ -0,0 +1,20 @@
+﻿#include"0000. Typedef.h"
+/*********************************************************************************
+  *函数： ReverseString
+  *描述： 利用顺序栈翻转给定的字符串sring
+  *输入：	char* string		待翻转的字符串string
+  *输出： 已翻转的字符串string
+  *返回：	无
+**********************************************************************************/
+void ReverseString(char* string) {
+	char SqStack[50];
+	int top = -1;
+	char* p = string;
+	while (*p != NULL) {
+		SqStack[++top] = *p;
+		p++;
+	}
+	while (top > -1) {
+		printf("%c", SqStack[top--]);
+	}
+}
